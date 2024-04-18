4ab1cf59ec4bf3f1ce862c7d67a3170b2df10d34
Jacob Harvey <jharvey@mahalobanking.com>
2024-04-18 09:59:40 -0600

    feat: create new server actions, forms, routing and wip create topic form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
-"use client";
-
-import { Button } from "@nextui-org/react";
-
-import * as actions from "@/actions";
-import Profile from "@/components/profile";
+import TopicCreateForm from "@/components/topics/topic-create-form";
 
 export default function Home() {
     return (
-        <div className="flex flex-col gap-6 p-4">
-            <form action={actions.signIn}>
-                <Button type="submit">SIGN IN</Button>
-            </form>
-            <form action={actions.signOut}>
-                <Button type="submit">SIGN OUT</Button>
-            </form>
-            <Profile />
+        <div className="grid grid-cols-4 gap-4 p-4">
+            <div className="col-span-3">
+                <h1 className="text-xl m-2">Top Posts</h1>
+            </div>
+            <div>
+                <TopicCreateForm />
+            </div>
         </div>
     );
 }
